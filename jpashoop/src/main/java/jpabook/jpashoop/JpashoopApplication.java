c1aee893af6f4cfa5152a8504c34a2b22c654cae
김성호 <nareun130@gmail.com>
2025-01-21 00:44:03 +0900

    feat : h2, jpa 설정

---
+++ jpashoop/src/main/java/jpabook/jpashoop/JpashoopApplication.java
@@ -0,0 +1,17 @@
+package jpabook.jpashoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashoopApplication {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println("data = " + data);
+		SpringApplication.run(JpashoopApplication.class, args);
+	}
+
+}
