1f3629de0c7a990e105cbc48ab624f328c9781b7
DESKTOP-AJCLGM9\nareun130 <nareun130@gmail.com>
2025-02-03 17:33:03 +0900

    feat : Fetch.LAZY를 추가

--- jpashoop/src/main/java/jpabook/jpashoop/domain/Category.java
+++ jpashoop/src/main/java/jpabook/jpashoop/domain/Category.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -32,7 +33,7 @@ public class Category {
     inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
--- jpashoop/src/main/java/jpabook/jpashoop/domain/Order.java
+++ jpashoop/src/main/java/jpabook/jpashoop/domain/Order.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -27,14 +28,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id") //! 외래키가 있는 곳 -> 연관 관계의 주인
     private Member member;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery")
     private Delivery delivery; //* 주로 액세스 하는 곳에 포린키를 둠. -> Order가 더 액세스를 많이 하므로, Order를 주인으로!!!
 
