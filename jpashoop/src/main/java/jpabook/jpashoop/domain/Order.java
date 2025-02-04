d96375445860ca3323508e766c5b1b4e1d811967
DESKTOP-AJCLGM9\nareun130 <nareun130@gmail.com>
2025-02-04 14:41:50 +0900

    feat : 연관관계 편의 메서드 추가

--- jpashoop/src/main/java/jpabook/jpashoop/domain/Order.java
+++ jpashoop/src/main/java/jpabook/jpashoop/domain/Order.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -21,26 +22,44 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "orders")
-@Getter @Setter
+@Getter
+@Setter
 public class Order {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "order_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id") //! 외래키가 있는 곳 -> 연관 관계의 주인
+    @JoinColumn(name = "member_id") // ! 외래키가 있는 곳 -> 연관 관계의 주인
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // * orderItems에 데이터를 넣고 Order를 저장하면, OrderItem들도 같이 저장됨.
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "delivery")
-    private Delivery delivery; //* 주로 액세스 하는 곳에 포린키를 둠. -> Order가 더 액세스를 많이 하므로, Order를 주인으로!!!
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery; // * 주로 액세스 하는 곳에 포린키를 둠. -> Order가 더 액세스를 많이 하므로, Order를 주인으로!!!
 
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
-    private OrderStatus status; //주문상태 [ORDER, CANCEL]
+    private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    // * 연관관계 편의 메서드 */ 
+    public void setMemeber(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
 }
